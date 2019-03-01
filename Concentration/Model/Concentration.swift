2b87848a2a281de00e08a65b041530b7d1843e60
Ales Shenshin <shenshin@me.com>
2019-03-01 03:42:25 +0300

    Add computed property indexOfOneAndOnlyFaceUpCard: Int?

--- Concentration/Model/Concentration.swift
+++ Concentration/Model/Concentration.swift
@@ -13,7 +13,27 @@ class Concentration {
     var cards: [Card] = []
 
     //индекс одной и только одной перевёрнутой вверх карты
-    var indexOfOneAndOnlyFaceUpCard: Int?
+    var indexOfOneAndOnlyFaceUpCard: Int? {
+        get {
+            var numberOfFaceUpCards: Int = 0
+            var indexOfFaceUpCard: Int?
+            for index in cards.indices where cards[index].isFaceUp == true {
+                numberOfFaceUpCards += 1
+                indexOfFaceUpCard = index
+            }
+            if numberOfFaceUpCards == 1 {
+                return indexOfFaceUpCard
+            } else {
+                return nil
+            }
+        }
+        set {
+            for index in cards.indices {
+                cards[index].isFaceUp = false
+            }
+            cards[newValue!].isFaceUp = true
+        }
+    }
 
     init(numberOfPairsOfCards: Int) {
         Card.identifierFactory = 0
@@ -26,6 +46,7 @@ class Concentration {
         //print(cards.map {$0.identifier}) //печатает элементы массива в строчку
         //cards.forEach {print($0.identifier)} //печатает элементы массива в столбик
     }
+    //TODO: - Переделать метод с учётом вычисляемого свойства indexOfOneAndOnlyFaceUpCard
     func chooseCard(at index: Int) {
         //если карта не помечена как угаданная
         if !cards[index].isMatched {
